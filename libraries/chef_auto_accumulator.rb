d0dd09e2b6fd0bb8beb26a47e50951781de1a56d
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2021-10-01 15:24:48 +0100

    Restructure into gem library format

---
+++ libraries/chef_auto_accumulator.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook:: chef_auto_accumulator
+# Library:: chef_auto_accumulator
+#
+# Copyright:: Ben Hughes <bmhughes@bmhughes.co.uk>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative 'chef_auto_accumulator/version'
+require_relative 'chef_auto_accumulator/_base'
+require_relative 'chef_auto_accumulator/_utils'
+
+require_relative 'chef_auto_accumulator/config'
+require_relative 'chef_auto_accumulator/file'
+require_relative 'chef_auto_accumulator/resource'
+
+# Base namespace
+module ChefAutoAccumulator; end
---
+++ libraries/chef_auto_accumulator/version.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook:: chef_auto_accumulator
+# Library:: version
+#
+# Copyright:: Ben Hughes <bmhughes@bmhughes.co.uk>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module ChefAutoAccumulator
+  cookbook_metadata = Chef::Cookbook::Metadata.new
+  cookbook_metadata.from_file("#{__dir__.gsub('libraries/chef_auto_accumulator', '')}/metadata.rb")
+
+  VERSION = cookbook_metadata.version.dup.freeze
+end
